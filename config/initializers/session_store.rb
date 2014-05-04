f45fb73fc5419f9b57a6a40df1c80d73d724c636
ichigotake <k.wisiiy@gmail.com>
2014-05-04 10:47:11 +0900

    Railsのセットアップ

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mikuregator::Application.config.session_store :cookie_store, key: '_Mikuregator_session'
--- db/schema.rb
+++ db/schema.rb
@@ -1,26 +1,16 @@
-CREATE TABLE authors
-(
-    id INT PRIMARY KEY NOT NULL,
-    name VARCHAR(128) NOT NULL,
-    avatar_image_url LONGTEXT NOT NULL,
-    created_at DATETIME NOT NULL,
-    updated_at DATETIME NOT NULL
-);
-CREATE TABLE repositories
-(
-    id INT PRIMARY KEY NOT NULL,
-    nickname VARCHAR(64) NOT NULL,
-    full_name VARCHAR(64) NOT NULL,
-    created_at DATETIME NOT NULL,
-    updated_at DATETIME NOT NULL,
-    description LONGTEXT NOT NULL,
-    api_resource_cache LONGTEXT NOT NULL,
-    html_url LONGTEXT NOT NULL,
-    repository_url LONGTEXT NOT NULL,
-    host_type CHAR(7) DEFAULT 'other' NOT NULL,
-    owner_author_id INT NOT NULL
-);
-CREATE UNIQUE INDEX unique_name ON authors ( name );
-ALTER TABLE repositories ADD FOREIGN KEY ( owner_author_id ) REFERENCES authors ( id );
-CREATE UNIQUE INDEX unique_full_name ON repositories ( full_name );
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 0) do
+
+end
